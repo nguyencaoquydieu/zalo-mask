9d234a39839b7b0cf99c1309b5a136f123484ec4
Dieu Nguyen <dieu.ncq@vinx.com.vn>
2025-07-16 15:23:14 +0700

    Use css for hide content and smooth animation

--- content.js
+++ content.js
@@ -1,63 +1,11 @@
-const MENTION_NAME_CLICKABLE = ['mention-name', 'clickable'];
-const TRUNCATE_QUOTE_NAME = ['truncate', 'quote-name'];
-const WRAPPER_IGNORE = ['.file-message-v2.file-message__container'];
-
-const TAGS = [
-	{
-		wrapperSelector: '.message-content-wrapper',
-		targetSelector: [
-			'span.text',
-			'div.message-quote-fragment__description',
-			'div.truncate.quote-name',
-			'div.truncate',
-			'a.mention-name.clickable',
-			'a.text-is-link',
-			'a.text-is-email',
-		]
-	},
-	{
-		wrapperSelector: '.zl-modal__dialog.flx.flx-col.animated.zoomIn',
-		targetSelector: ['div.truncate']
-	},
-	{
-		wrapperSelector: '.flx.flx-center.slideshow',
-		targetSelector: ['div.truncate']
-	},
+const STYLE_HIDDEN_TEXT = 'hidden-text';
+const SELECTORS = [
+	'[data-id="div_DisabledTargetEventLayer"]',
+	'.conv-item-title__name.truncate.grid-item',
+	'.chat-group-topic__item',
+	'.threadChat__title.flx-1.flx.flx-col.w0',
+	'.conv-item-body.flx.flx-al-c.truncate.grid-item',
 ];
-
-// const TAGS = [
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'span.text' },
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'div.message-quote-fragment__description' },
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'div.truncate.quote-name' },
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'div.truncate' },
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'a.mention-name.clickable' },
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'a.text-is-link' },
-//   { wrapperSelector: '.message-content-wrapper', targetSelector: 'a.text-is-email' },
-//   { wrapperSelector: '.zl-modal__dialog.flx.flx-col.animated.zoomIn', targetSelector: 'div.truncate' },
-//   { wrapperSelector: '.flx.flx-center.slideshow', targetSelector: 'div.truncate' },
-// ];
-
-
-
-const HIDE_DIGIT = '•';
-const REACT_VIRTUALIZED_GRID = 'ReactVirtualized__Grid__innerScrollContainer';
-const MESSAGE_VIEW_SCROLL_INNER = 'message-view__scroll__inner';
-const REL = 'rel';
-const MESSAGE_VIEW = 'message-view';
-const CHAT_CONTENT = 'chat-content';
-const BLOCK_DATE = 'block-date';
-
-const MSG_ITEM = '.msg-item';
-const TRUNCATE = '.truncate';
-const MESSAGE_CONTENT_WRAPPER = 'message-content-wrapper';
-const SPAN_TEXT = 'span.text';
-
-const CONV_ITEM_TITLE_NAME = 'conv-item-title__name';
-const CONV_ITEM_BODY = 'conv-item-body';
-const TRANSFORM_GPU = 'transform-gpu';
-const MESSAGE_ACTION = 'message-action';
-
-
 
 const observer = new MutationObserver((mutationsList) => {
 	processMutationList(mutationsList);
@@ -67,138 +15,49 @@ observer.observe(document.body, { childList: true, subtree: true });
 function processMutationList(mutationsList) {
 	if (mutationsList) {
 		mutationsList.forEach(mutation => {
-			processAddedNodes(mutation.addedNodes);
-			processTargetNode(mutation.target);
-			processNextSiblingNode(mutation.nextSibling);
+			processMutation(mutation);
 		});
 	}
 }
 
-function processAddedNodes(addedNodes) {
-	if (addedNodes && addedNodes.length > 0) {
-		addedNodes.forEach(node => {
-			processNodeElement(node);
-		});
-	}
-}
-
-function processTargetNode(node) {
-	if (node) {
-		if (node.classList) {
-			if (node.classList.contains(REL)
-				|| node.classList.contains(MESSAGE_VIEW)
-				|| node.classList.contains(CHAT_CONTENT)
-				|| node.classList.contains(BLOCK_DATE)) {
-				elements = node.querySelectorAll('.' + MESSAGE_VIEW_SCROLL_INNER);
-				if (elements) {
-					elements.forEach(element => {
-						processTargetNode(element);
-					});
-				}
-			}
-
-			if (node.classList.contains(REACT_VIRTUALIZED_GRID)
-				|| node.classList.contains(MESSAGE_VIEW_SCROLL_INNER)
-				|| node.classList.contains(MESSAGE_CONTENT_WRAPPER)) {
-				processNodeElement(node);
-			}
+function processMutation(mutation) {
+	if (mutation) {
+		const nodes = [];
+		if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+			nodes.push(...mutation.addedNodes);
 		}
-	}
-}
 
-function processNextSiblingNode(node) {
-	if (node && node.classList && node.classList.contains(MESSAGE_ACTION)) {
-		processNodeElement(node);
+		if (mutation.target) {
+			nodes.push(mutation.target);
+		}
+
+		if (mutation.nextSibling) {
+			nodes.push(mutation.nextSibling);
+		}
+
+		if (nodes.length > 0) {
+			nodes.forEach(node => {
+				processNodeElement(node);
+			});
+		}
 	}
 }
 
 function processNodeElement(node) {
-	if (node && node.nodeType === Node.ELEMENT_NODE) {
-		if (node.classList) {
-			processElementReactVirtualizedGrid(node);
-			processElementMessageContentWrapper(node);
-			processElementMessageAction(node);
-		}
+	if (node && node.nodeType == Node.ELEMENT_NODE) {
+		SELECTORS.forEach(selector => {
+			const elements = node.querySelectorAll(selector);
+			if (elements) {
+				elements.forEach(element => {
+					processHideDigit(element);
+				});
+			}
+		});
 	}
 }
 
-function processElementReactVirtualizedGrid(node) {
-	if (node.classList.contains(REACT_VIRTUALIZED_GRID)) {
-		const elements = node.querySelectorAll(`${MSG_ITEM} ${TRUNCATE}`);
-		if (elements) {
-			elements.forEach(element => {
-				if (!element.dataset.maskHoverBound) {
-					let length = 0;
-					if (element.classList.contains(CONV_ITEM_TITLE_NAME)) {
-						length = 10;
-					} else if (element.classList.contains(CONV_ITEM_BODY)) {
-						length = 11;
-					}
-
-					if (length > 0) {
-						element.textContent = HIDE_DIGIT.repeat(length);
-						element.dataset.maskHoverBound = true;
-					}
-				}
-			});
-		}
-	}
-}
-
-function processElementMessageContentWrapper(node) {
-	if (node.classList.contains(MESSAGE_VIEW_SCROLL_INNER)) {
-		const elements = node.querySelectorAll('.' + MESSAGE_CONTENT_WRAPPER);
-		if (elements) {
-			elements.forEach(element => {
-				if (!element.dataset.isNotFirstTime) {
-					processEventListener(null, false, element);
-					element.dataset.isNotFirstTime = true;
-				}
-
-				if (!element.dataset.eventAttached) {
-					element.addEventListener('mouseenter', (event) => { processEventListener(event, true); });
-					element.addEventListener('mouseleave', (event) => { processEventListener(event, false); });
-					element.dataset.eventAttached = true;
-				}
-			});
-		}
-	}
-}
-
-function processElementMessageAction(node) {
-	if (node.classList.contains(MESSAGE_ACTION)) {
-		const element = node.closest(MESSAGE_VIEW_SCROLL_INNER);
-		if (element && element.classList) {
-			processElementMessageContentWrapper(element);
-		}
-	}
-}
-
-function processEventListener(event, isShowDigit, element = null) {
-	if (element || (event && event.target)) {
-		const elements = element ? element.querySelectorAll(SPAN_TEXT) : event.target.querySelectorAll(SPAN_TEXT);
-		if (elements) {
-			elements.forEach(element => {
-				if (isShowDigit) {
-					showDigit(element);
-				} else {
-					hideDigit(element);
-				}
-			});
-		}
-	}
-}
-
-function hideDigit(element) {
-	if (element && !element.dataset.originalText) {
-		element.dataset.originalText = element.textContent;
-		element.textContent = HIDE_DIGIT.repeat(element.textContent.length);
-	}
-}
-
-function showDigit(element) {
-	if (element && element.dataset.originalText) {
-		element.textContent = element.dataset.originalText;
-		element.dataset.originalText = '';
+function processHideDigit(element) {
+	if (element && element.classList && !element.classList.contains(STYLE_HIDDEN_TEXT)) {
+		element.classList.add(STYLE_HIDDEN_TEXT);
 	}
 }
